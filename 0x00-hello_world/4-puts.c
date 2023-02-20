31c81706fa7bd8de57c6eb3aa49b68e12df2c414
WaweruMuraya <vincemuraya@gmail.com>
2023-02-19 18:24:56 -0800

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include<stdio.h>
 /**
  * main - Entry point
@@ -6,8 +5,8 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{       
-        puts("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-} 
+}
 
